2fc9db3d2fc26bc5f708514d5343c259a7d76e5e
Slenderpi <PrestonFu09@gmail.com>
2026-09-19 17:00:28 -0700

    DevConfig, DevGui

---
+++ core/globals/dev_tools/dev_config.gd
@@ -0,0 +1,181 @@
+extends Node
+## Manages/contains info from developer_config.cfg
+
+
+#region CONFIG FILE STRINGS
+
+## The .cfg file where developer config values are stored.
+## The file is stored in your "user" folder,[br]
+## which you can open via [b]Project > Open User Data Folder[/b].[br]
+## [br]
+## You can manually find this folder at the following location:[br]
+## Windows: %APPDATA%[br]
+## Mac: ~/Library/Application Support/[br]
+## Linux: ~/.local/share/
+const CFG_PATH := "user://developer_config.cfg"
+
+const SECTION_GENERAL := "general"
+const CFGKEY_FREEPLAY_MODE = "freeplay_mode"
+
+const SECTION_GRAPHICS := "graphics"
+
+const SECTION_DEV_GUI_CONFIG := "dev_gui_config"
+const CFGKEY_PAUSE_ON_DEV_GUI := "pause_on_dev_gui"
+const CFGKEY_DEV_GUI_ENABLED := "dev_gui_enabled"
+
+#endregion
+
+
+#region CONFIG VALUES
+## HOW TO USE:
+## For a new config value, add a constant CFGKEY_KEYNAME string into the CONFIG FILE region,
+## then put it as a key in a dictionary below.
+## Initialize its value to an appropriate default value.
+## Then add the dictionary to the `configs` dictionary with the appropriate section string.
+
+## Contains all section configs.
+## If you add a new configs dictionary, make sure you add it into [configs] by
+## adding a line in [method DevConfig._init_configs_dict]
+var configs : Dictionary[String, Dictionary]
+
+var general_config : Dictionary[String, Variant] = {
+	CFGKEY_FREEPLAY_MODE: false,
+}
+
+## Dictionary of cfg keys and their values, specific to graphics.
+var graphics_config : Dictionary[String, Variant] = {
+	
+}
+
+## Dictionary of cfg keys and their values, specific to dev gui config.
+var dev_gui_config_config : Dictionary[String, Variant] = {
+	CFGKEY_DEV_GUI_ENABLED: false,
+	CFGKEY_PAUSE_ON_DEV_GUI: true,
+}
+
+#endregion
+
+
+#region INITIALIZERS
+
+func _ready() -> void:
+	_init_configs_dict()
+	_init_config_values()
+
+
+## Initializes [member DevConfigs.configs] with each section-specific sub-configs dictionary.
+## Add a line if you create a new sub-config dictionary.
+func _init_configs_dict() -> void:
+	configs = {
+		SECTION_GENERAL: general_config,
+		SECTION_GRAPHICS: graphics_config,
+		SECTION_DEV_GUI_CONFIG: dev_gui_config_config,
+	}
+
+
+# Initializes the subdictionaries of configs to their values in the config file, if found.
+# Defaults to the default value you set in the initialization of your dictionary.
+func _init_config_values() -> void:
+	var cfg := ConfigFile.new()
+	cfg.load(CFG_PATH)
+	for section in configs:
+		var currConfig : Dictionary[String, Variant] = configs[section]
+		for key in currConfig:
+			currConfig[key] = cfg.get_value(section, key, currConfig[key])
+
+#endregion
+
+
+#region GET VALUE
+
+## Shorthand for [method DevConfig.get_value],
+## where [code]section[/code] is given [member DevConfig.SECTION_GENERAL].
+func get_general_value(cfgKey: String, default: Variant = null) -> Variant:
+	return get_value(SECTION_GENERAL, cfgKey, default)
+
+
+## Shorthand for [method DevConfig.get_value],
+## where [code]section[/code] is given [member DevConfig.SECTION_GRAPHICS].
+func get_graphics_value(cfgKey: String, default: Variant = null) -> Variant:
+	return get_value(SECTION_GRAPHICS, cfgKey, default)
+
+
+## Shorthand for [method DevConfig.get_value],
+## where [code]section[/code] is given [member DevConfig.SECTION_DEV_GUI_CONFIG].
+func get_dev_gui_config_value(cfgKey: String, default: Variant = null) -> Variant:
+	return get_value(SECTION_DEV_GUI_CONFIG, cfgKey, default)
+
+
+## Get a value from [member DevConfigs.configs].
+## If the value cannot be found, the config file [member DevConfig.CFG_PATH]
+## is searched instead.
+func get_value(section: String, cfgKey: String, default: Variant = null) -> Variant:
+	# Try finding it in configs
+	if configs.has(section):
+		var currCfg := configs[section]
+		if currCfg.has(cfgKey):
+			return currCfg[cfgKey]
+	
+	# Fallback to searching the config file
+	var cfg := ConfigFile.new()
+	cfg.load(CFG_PATH)
+	return cfg.get_value(section, cfgKey, default)
+
+#endregion
+
+
+#region SET VALUE
+
+## Shorthand for [method DevConfig.get_value],
+## where [member DevConfig.SECTION_GENERAL] is the section parameter.
+func set_general_value(cfgKey: String, value: Variant) -> Error:
+	return set_value(SECTION_GENERAL, cfgKey, value)
+
+
+## Shorthand for [method DevConfig.get_value],
+## where [member DevConfig.SECTION_GRAPHICS] is the section parameter.
+func set_graphics_value(cfgKey: String, value: Variant) -> Error:
+	return set_value(SECTION_GRAPHICS, cfgKey, value)
+
+
+## Shorthand for [method DevConfig.get_value],
+## where [member DevConfig.SECTION_DEV_GUI_CONFIG] is the section parameter.
+func set_dev_gui_config_value(cfgKey: String, value: Variant) -> Error:
+	return set_value(SECTION_DEV_GUI_CONFIG, cfgKey, value)
+
+
+## Set a value and save it to [member DevConfig.CFG_PATH].
+func set_value(section: String, cfgKey: String, value: Variant) -> Error:
+	configs[section][cfgKey] = value
+	var cfg := ConfigFile.new()
+	cfg.load(CFG_PATH)
+	cfg.set_value(section, cfgKey, value)
+	var err := cfg.save(CFG_PATH)
+	if err != OK:
+		printerr("[DevConfig] Failed to save config file \"%s\" with error code %d." % [CFG_PATH, err])
+	return err
+
+#endregion
+
+
+#region MISC CONFIG FUNCTIONS
+
+
+## Deletes the config file [member DevConfig.CFG_PATH].[br]
+## [br]
+## The cached [member DevConfig.configs] dictionaries are not affected by this method.
+func delete_config_file() -> void:
+	if does_config_file_exist():
+		var err := DirAccess.remove_absolute(CFG_PATH)
+		if err == OK:
+			print("[DevConfig] Your developer config file has successfully been deleted.")
+		else:
+			printerr("[DevConfig] Unable to delete config file! Error code: %d" % err)
+	else:
+		print("[DevConfig] The config file does not exist.")
+
+
+## Use this to check if the dev config file located at [member DevConfig.CFG_PATH]
+## exists.
+func does_config_file_exist() -> bool:
+	return FileAccess.file_exists(CFG_PATH)
---
+++ core/globals/dev_tools/dev_gui.gd
@@ -0,0 +1,221 @@
+extends Node
+## Provides a gui for us to do dev actions, using [ImGui].
+
+
+## Determines if the main DevGui window is showing or not.
+var dev_gui_enabled := false
+
+# Fps text is yellow if fps > this value, else red.
+const MODERATE_FPS_LOWER_LIMIT := 30.0
+# Fps text is green if fps > this value, else yellow.
+const GOOD_FPS_LOWER_LIMIT := 50.0
+
+#var sceneNameList : Array[String]
+#var changeSceneChoice : int = 0
+#var longestSceneName : int = 0
+
+var _mouse_visibility_before_show := Input.MOUSE_MODE_VISIBLE
+var _was_resumed_before_dev_gui := false
+
+var _pause_on_dev_gui := true
+var _is_deleting_cfg_file := false
+
+
+func _ready() -> void:
+	process_mode = Node.PROCESS_MODE_ALWAYS
+	dev_gui_enabled = DevConfig.get_dev_gui_config_value(DevConfig.CFGKEY_DEV_GUI_ENABLED)
+	_pause_on_dev_gui = DevConfig.get_dev_gui_config_value(DevConfig.CFGKEY_PAUSE_ON_DEV_GUI, _pause_on_dev_gui)
+	#sceneNameList = SceneManager.SCENE_PATHS.keys()
+	#for sn in sceneNameList:
+		#if sn.length() > longestSceneName:
+			#longestSceneName = sn.length()
+
+
+func _process(delta: float) -> void:
+	if not dev_gui_enabled:
+		return
+	ImGui.Begin("ARCADE GAME DevGui") # Can add a version number
+	_imgui_consistent_info(delta)
+	#ImGui.BeginDisabled(SceneManager.is_transitioning)
+	if ImGui.BeginTabBar("Categories"):
+		if ImGui.BeginTabItem("General"):
+			_imgui_tab_general()
+			ImGui.EndTabItem()
+		if ImGui.BeginTabItem("Graphics"):
+			_imgui_tab_graphics()
+			ImGui.EndTabItem()
+		if ImGui.BeginTabItem("DevGui Config"):
+			_imgui_tab_devgui_config()
+			ImGui.EndTabItem()
+		ImGui.EndTabBar()
+	#ImGui.EndDisabled()
+	ImGui.End()
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("toggle_dev_gui"):
+		dev_gui_enabled = !dev_gui_enabled
+		DevConfig.set_dev_gui_config_value(DevConfig.CFGKEY_DEV_GUI_ENABLED, dev_gui_enabled)
+		if dev_gui_enabled:
+			_mouse_visibility_before_show = Input.mouse_mode
+			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+			_was_resumed_before_dev_gui = not get_tree().paused
+			if _pause_on_dev_gui:
+				get_tree().paused = true
+		else:
+			Input.mouse_mode = _mouse_visibility_before_show
+			if _pause_on_dev_gui and _was_resumed_before_dev_gui:
+				get_tree().paused = false
+
+
+#region OUTSIDE TABS
+
+func _imgui_consistent_info(delta: float) -> void:
+	ImGui.Text("FPS:")
+	ImGui.SameLine()
+	
+	var liveFps := 1.0 / delta
+	var fpsColor := Color.GREEN if liveFps > GOOD_FPS_LOWER_LIMIT else (Color.YELLOW if liveFps > MODERATE_FPS_LOWER_LIMIT else Color.RED)
+	ImGui.TextColored(fpsColor, "%3d" % roundi(Engine.get_frames_per_second()))
+	ImGui.SameLine()
+	ImGui.Text('=')
+	ImGui.SameLine()
+	ImGui.TextColored(fpsColor, "%5.1f ms" % (round(delta * 10000.0) / 10.0))
+	ImGui.SameLine()
+	
+	ImGui.Text("|")
+	ImGui.SameLine()
+	
+	ImGui.Text("<other persistent info>")
+
+#endregion
+
+
+#region TAB GENERAL
+
+func _imgui_tab_general() -> void:
+	ImGui.TextWrapped("This will become a general tab with often-used information and capabilities.")
+	
+	_imgui_general_credits()
+	#_imgui_general_scene_changing()
+
+
+func _imgui_general_credits() -> void:
+	var tempArr := []
+	
+	tempArr = [DevConfig.get_general_value(DevConfig.CFGKEY_FREEPLAY_MODE)]
+	if ImGui.Checkbox("Freeplay mode", tempArr):
+		var fpMode : bool = tempArr[0]
+		CreditManager.set_freeplay_mode(fpMode)
+		DevConfig.set_general_value(DevConfig.CFGKEY_FREEPLAY_MODE, fpMode)
+
+
+#func _imgui_general_scene_changing() -> void:
+	#var tempArr := []
+	#if ImGui.Button("Change scene to:"):
+		#SceneManager.change_scene_to(sceneNameList[changeSceneChoice])
+	#ImGui.SameLine()
+	#tempArr = [changeSceneChoice]
+	#ImGui.SetNextItemWidth(ImGui.GetFontSize() * longestSceneName)
+	#if ImGui.Combo("Scene name", tempArr, sceneNameList):
+		#changeSceneChoice = tempArr[0]
+
+#endregion
+
+
+#region TAB GRAPHICS
+
+func _imgui_tab_graphics() -> void:
+	#if ImGui.CollapsingHeader("Shaders"):
+	ImGui.SeparatorText("Screen Settings")
+	_imgui_screen_settings()
+	#ImGui.SeparatorText("Shaders")
+	#_imgui_graphics_shaders()
+
+
+func _imgui_screen_settings() -> void:
+	var tempArr: Array = []
+	
+	tempArr = [DisplayServer.window_get_vsync_mode()]
+	if ImGui.Checkbox("VSync", tempArr):
+		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_ENABLED if tempArr[0] else DisplayServer.VSYNC_DISABLED)
+	if ImGui.IsItemHovered(ImGui.HoveredFlags_DelayNone):
+		ImGui.SetTooltip(
+			"VSync is %s. Click to change it to %s." %
+			[_bool_text_enabled(tempArr[0]), _bool_text_enabled(!tempArr[0])]
+		)
+
+
+#func _imgui_graphics_shaders() -> void:
+	#var tempArr: Array = []
+	#
+	#tempArr = [DevConfig.get_graphics_value(DevConfig.CFGKEY_OUTLINE)]
+	#if ImGui.Checkbox("Outline visibility", tempArr):
+		#Debug.graphics_set_outline_shader(tempArr[0])
+	#if ImGui.IsItemHovered(ImGui.HoveredFlags_DelayNone):
+		#ImGui.SetTooltip("Toggles the outline shader.")
+
+#endregion
+
+
+#region TAB DEVGUI CONFIG
+
+func _imgui_tab_devgui_config() -> void:
+	var tempArr: Array = []
+	ImGui.TextWrapped("This will contain settings for DevGui itself.")
+	# Examples: pause on DevGui show, free mouse on DevGui show
+	
+	tempArr = [_pause_on_dev_gui]# [DevConfig.get_dev_gui_config_value(DevConfig.CFGKEY_PAUSE_ON_DEV_GUI)]
+	if ImGui.Checkbox("Pause on DevGui", tempArr):
+		_pause_on_dev_gui = tempArr[0]
+		DevConfig.set_dev_gui_config_value(DevConfig.CFGKEY_PAUSE_ON_DEV_GUI, _pause_on_dev_gui)
+		if _pause_on_dev_gui:
+			get_tree().paused = true
+		elif _was_resumed_before_dev_gui:
+			get_tree().paused = false
+	if ImGui.IsItemHovered(ImGui.HoveredFlags_DelayNone):
+		ImGui.SetTooltip("Determines if the game will get paused when DevGui is opened.")
+	
+	ImGui.BeginDisabled(not DevConfig.does_config_file_exist())
+	ImGui.PushStyleColor(ImGui.Col_Button, Color(0.6, 0.0, 0.0, 1.0))
+	ImGui.PushStyleColor(ImGui.Col_ButtonHovered, Color(0.85, 0.136, 0.148, 1.0))
+	ImGui.PushStyleColor(ImGui.Col_ButtonActive, Color(0.85, 0.391, 0.399, 1.0))
+	if ImGui.Button("Clear config file"):
+		_is_deleting_cfg_file = true
+	ImGui.PopStyleColorEx(3)
+	if _is_deleting_cfg_file:
+		ImGui.OpenPopup("Confirm Deletion")
+		ImGui.SetNextWindowPos(get_viewport().get_mouse_position(), ImGui.Cond_Appearing)
+		# Might be worth putting the below popup code in a dedicated confirmation-popup-creating function
+		if ImGui.BeginPopupModal("Confirm Deletion", [], ImGui.WindowFlags_AlwaysAutoResize):
+			ImGui.Text("Delete developer config file?")
+			ImGui.Text("Currently cached config values will not be affected.")
+			ImGui.Separator()
+			if ImGui.Button("Confirm"):
+				DevConfig.delete_config_file()
+				_is_deleting_cfg_file = false
+				ImGui.CloseCurrentPopup()
+			ImGui.SameLine()
+			if ImGui.Button("CANCEL"):
+				_is_deleting_cfg_file = false
+				ImGui.CloseCurrentPopup()
+		ImGui.EndPopup()
+	if ImGui.IsItemHovered(ImGui.HoveredFlags_DelayNone):
+		ImGui.SetTooltip("Deletes your developer config file.\nPath: %s" % DevConfig.CFG_PATH)
+	ImGui.EndDisabled()
+
+#endregion
+
+
+#region HELPER FUNCTIONS
+
+func _bool_color(b: bool) -> Color:
+	return Color.GREEN if b else Color.RED
+
+func _bool_text_enabled(b: bool) -> String:
+	return "ENABLED" if b else "DISABLED"
+
+func _bool_text_on(b: bool) -> String:
+	return "ON" if b else "OFF"
+
+#endregion
